d77d5fe4f5539e7af1f744058cc8a29e5d62a7b7
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2022-05-24 14:35:06 +0800

    search function finished

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
+    $(document).on('click', '.search-dropdown-item', function() {
+        determinePlaceholder(this.getAttribute('id'))
+    });
     var clicks = 0
     var articles = []
+    var searchCondi = [
+        ['searchAll', '全域搜尋'],
+        ['searchByID', '依照ID'],
+        ['searchByTitle', '依照標題'],
+        ['searchByContent', '依照內容'],
+        ['searchByDate', '依照日期'],
+        ['searchByAuthor', '依照作者']
+    ]
+    var searchBy = "searchAll"
     card_template = `
     <div class="card">
       <div class="card-body">
@@ -16,50 +28,107 @@ $(document).ready(function() {
         if (xhr.readyState === 4) {
             articles = JSON.parse(xhr.responseText)
         }
-
     }
     xhr.send();
 
-    function renderList(articleList) {
-
-        for (x of articleList) {
-            if (x.hidden == "false") {
-                $('#container').prepend(`
-            </br>
-    <div id="${x.id}" class="card post" onclick="localStorage.setItem('targetid', $(this).attr('id'));window.open('./article/');">
-      <div class="card-body">
-        <h4 style="font-weight:500;" class="card-title">${x.title}</h4>
-        <h6 class="text-secondary">由 ${x.author} 於 ${x.date} 發佈</h6>
-        <h5 style="font-weight:300;" class="card-text omit">${x.content}</h5>
-        <h6 style="font-weight:300;" class="text-secondary">#${x.id}<h6>
-      </div>
-    </div>
-    
-    `);
-            }
-
-
+    function determinePlaceholder(elementID) {
+        if (elementID == "searchByDate") {
+            $('#search-input').attr('placeholder', 'YYYY-MM-DD');
+        } else {
+            $('#search-input').attr('placeholder', '搜尋');
         }
     }
 
-    $('#dropdown-menu-01-01').on('click', function() {
-        clicks++
-
-        if (clicks < 4) {
-            alert('真的沒了\(￣︶￣*\))')
-        } else {
-            if (clicks < 7) {
-                alert('我真的沒騙你')
-            } else {
-                if (clicks < 10) {
-                    alert('別點了...')
-                } else {
-                    alert("沒救了...")
-                }
+    function renderPost(articleList) {
+        $('#container').empty();
+        for (x of articleList) {
+            if (!x.hidden) {
+                $('#container').prepend(`
+                    </br>
+                    <div id="${x.id}" class="card post" onclick="localStorage.setItem('targetid', $(this).attr('id'));window.open('./article/');">
+                        <div class="card-body">
+                            <h4 style="font-weight:500;" class="card-title">${x.title}</h4>
+                            <h6 class="text-secondary">由 ${x.author} 於 ${x.date} 發佈</h6>
+                            <h5 style="font-weight:300;" class="card-text omit">${x.content}</h5>
+                            <h6 style="font-weight:300;" class="text-secondary">#${x.id}<h6>
+                        </div>
+                    </div>
+                `);
             }
         }
+    }
+
+    function renderSearch(dropdownList) {
+        for (x of dropdownList) {
+            $('#search-dropdown').append(`
+                <p id="${x[0]}" class="search-dropdown-item dropdown-item" onclick="searchBy=$(this).attr('id');">${x[1]}</p>
+            `);
+        }
+    }
+
+    function findPost(keyWord, articleList) {
+        temp = []
+        reg = new RegExp(keyWord, 'i')
+
+        if (searchBy == 'searchByID') {
+            articleList.forEach(function(d) {
+                if (d.id == keyWord) {
+                    temp.push(d)
+                }
+
+            });
+        }
+
+        if (searchBy == 'searchByTitle') {
+            articleList.forEach(function(d) {
+                if (reg.test(d.title)) {
+                    temp.push(d)
+                }
+            });
+        }
+
+        if (searchBy == 'searchByContent') {
+            articleList.forEach(function(d) {
+                if (reg.test(d.content)) {
+                    temp.push(d)
+                }
+            });
+        }
+
+        if (searchBy == 'searchByDate') {
+            articleList.forEach(function(d) {
+                if (d.date == keyWord) {
+                    temp.push(d)
+                }
+            });
+        }
+        if (searchBy == 'searchByAuthor') {
+            articleList.forEach(function(d) {
+                if (d.author == keyWord) {
+                    temp.push(d)
+                }
+            });
+        }
+
+        if (searchBy == 'searchAll') {
+            articleList.forEach(function(d) {
+                if (reg.test(d.title) || reg.test(d.content) || reg.test(d.id) || reg.test(d.date)) {
+                    temp.push(d)
+                }
+            });
+        }
+        //先推入temp最後統一更新
+        renderPost(temp)
+            // console.log(temp)
+    }
+
+    $('#searchByDate').on('click', function() {
+        console.log($(this))
     });
+
+    renderSearch(searchCondi);
+
     setTimeout(() => {
-        renderList(articles);
+        renderPost(articles);
     }, 300);
 });
